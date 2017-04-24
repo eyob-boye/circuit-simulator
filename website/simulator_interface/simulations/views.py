6e3f378fe07c207b6f277c3a0caf1098ad80022d
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2017-04-24 17:20:07 -0400

    Still working with the simulation parameters form. Segregated the basic parameters from the circuit file uploads.
    Once a circuit file is uploaded, the file appears only as a name in a list. Which is good enough. The only challenge is in ensuring that anny filesystem whether windows or linux can read every circuit file without any problems.

--- website/simulator_interface/simulations/views.py
+++ website/simulator_interface/simulations/views.py
@@ -12,30 +12,14 @@ def new_simulation(request):
     if not request.method == "POST":
         simulation_form = []
         simulation_form.append(SimulationCaseForm())
-        simulation_form.append(CircuitSchematicsForm())
+        ckt_schematic_form = []
+        ckt_schematic_form.append([[], CircuitSchematicsForm()])
 
     else:
         if "submit" in request.POST and request.POST["submit"]=="Submit":
-            print(request.POST)
-            print
-            print
-            print(request.POST.getlist(u'ckt_file_path'))
-#            print(request.FILES[u'ckt_file_path'])
-#            try_file = CircuitSchematics()
-#            try_file.ckt_file_path = request.FILES[u'ckt_file_path']
-#            print(try_file)
-#            print(try_file.ckt_file_path.path)
-#            print(try_file.ckt_file_path.name)
-#            try_file.path()
-#            try_file.save()
-#            print
-#            print
             sim_para_received = SimulationCaseForm(request.POST)
             if sim_para_received.is_valid():
                 sim_parameters = sim_para_received.cleaned_data
-                print(sim_parameters)
-                print
-                print
                 sim_para_model = SimulationCase()
                 sim_para_model.sim_title = sim_parameters["sim_title"]
                 sim_para_model.sim_descrip = sim_parameters["sim_descrip"]
@@ -47,15 +31,20 @@ def new_simulation(request):
                 sim_para_model.sim_working_directory = sim_parameters["sim_working_directory"]
                 
                 ckt_file = CircuitSchematics()
-                ckt_file.ckt_file_name = request.POST.getlist(u'ckt_file_path')[0]
+                if u'ckt_file_path' in request.POST:
+                    ckt_file.ckt_file_name = request.POST.getlist(u'ckt_file_path')[0]
+                if u'ckt_file_descrip' in request.POST:
+                    ckt_file.ckt_file_descrip = request.POST.getlist(u'ckt_file_descrip')[0]
                 ckt_file.save()
                 sim_para_model.sim_circuit_files = ckt_file
                 sim_para_model.save()
             
-            simulation_form = []
-            simulation_form.append(SimulationCaseForm(instance=sim_para_model))
-            simulation_form.append(CircuitSchematicsForm(instance=ckt_file))
+                simulation_form = []
+                simulation_form.append(SimulationCaseForm(instance=sim_para_model))
+                ckt_schematic_form = []
+                ckt_schematic_form.append([ckt_file, CircuitSchematicsForm(instance=ckt_file)])
 
     return render(request,
           "new_simulation.html",
-          {'simulation_form' : simulation_form})
+          {'simulation_form' : simulation_form,
+           'ckt_schematic_form' : ckt_schematic_form})
