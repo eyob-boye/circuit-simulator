32183df5c420d153a292f994be66af05ab5ce2f2
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2017-04-27 23:45:40 -0400

    The code is cleaner and also the form. The page doesn't contain all form elements which might confuse the user. It is broken up into smaller segments with separate buttons.
    The form and the view now requires significant bookkeeping to make sure the webpage generates the right information. This might have to be made a bit easier. Also the deletion of circuit schematics requires a bit of work which can be simplified.

--- website/simulator_interface/simulations/views.py
+++ website/simulator_interface/simulations/views.py
@@ -29,12 +29,19 @@ def extract_simulation_case(request):
         sim_para_model.sim_div_number = sim_parameters["sim_div_number"]
         sim_para_model.sim_working_directory = sim_parameters["sim_working_directory"]
         sim_para_model.save()
+        sim_id = sim_para_model.id
 
         simulation_form = []
-        simulation_form.append([sim_para_model])
-        simulation_form.append([SimulationCaseForm(instance=sim_para_model)])
+        simulation_form.append(sim_para_model)
+        simulation_form.append([])
+
     else:
-        pass
+        sim_id = -1
+        simulation_form = []
+        simulation_form.append([])
+        simulation_form.append(sim_para_received)
+
+    return [sim_id, simulation_form]
 
 
 def index(request):
@@ -45,108 +52,80 @@ def new_simulation(request):
     if not request.method == "POST":
         sim_id = -1
         simulation_form = []
+        simulation_form.append([])
         simulation_form.append(SimulationCaseForm())
         ckt_schematic_form = []
-        ckt_schematic_form.append([[], CircuitSchematicsForm()])
 
     else:
         print(request.POST)
-        print(repr(request.POST["sim_working_directory"]))
         print
         print
+        if "save_ckt_schematic" in request.POST and request.POST["save_ckt_schematic"]=="Save circuit file":
+            if "sim_id" in request.POST:
+                sim_id = int(request.POST["sim_id"])
+                if sim_id>0:
+                    sim_para_model = SimulationCase.objects.get(id=sim_id)
+    
+            if u'ckt_file_path' in request.POST:
+                ckt_file = CircuitSchematics()
+                ckt_file.ckt_file_name = request.POST.getlist(u'ckt_file_path')[0]
+                if u'ckt_file_descrip' in request.POST:
+                    ckt_file.ckt_file_descrip = request.POST.getlist(u'ckt_file_descrip')[0]
+                ckt_file.ckt_sim_case = sim_para_model
+                ckt_file.save()
+    
+    
+            simulation_form = []
+            simulation_form.append(sim_para_model)
+            simulation_form.append([])
+            ckt_file_list = sim_para_model.circuitschematics_set.all()
+            ckt_schematic_form = []
+            for ckt_file_item in ckt_file_list:
+                ckt_schematic_form.append([ckt_file_item, CircuitSchematicsForm(instance=ckt_file_item)])
+#            ckt_schematic_form.append([[], CircuitSchematicsForm()])
+
         if "add_ckt_schematic" in request.POST and request.POST["add_ckt_schematic"]=="Add circuit file":
-            sim_para_received = SimulationCaseForm(request.POST)
-            if sim_para_received.is_valid():
-                sim_parameters = sim_para_received.cleaned_data
-                if "sim_id" in request.POST:
-                    sim_id = int(request.POST["sim_id"])
-                    if sim_id>0:
-                        sim_para_model = SimulationCase.objects.get(id=sim_id)
-                    else:
-                        sim_para_model = SimulationCase()
-                sim_para_model.sim_title = sim_parameters["sim_title"]
-                sim_para_model.sim_descrip = sim_parameters["sim_descrip"]
-                sim_para_model.sim_time_limit = sim_parameters["sim_time_limit"]
-                sim_para_model.sim_time_data = sim_parameters["sim_time_data"]
-                sim_para_model.sim_output_file = sim_parameters["sim_output_file"]
-                sim_para_model.sim_output_slice = sim_parameters["sim_output_slice"]
-                sim_para_model.sim_div_number = sim_parameters["sim_div_number"]
-                sim_para_model.sim_working_directory = sim_parameters["sim_working_directory"]
-                sim_para_model.save()
-                sim_id = sim_para_model.id
+            if "sim_id" in request.POST:
+                sim_id = int(request.POST["sim_id"])
+                if sim_id>0:
+                    sim_para_model = SimulationCase.objects.get(id=sim_id)
+    
+            if u'ckt_file_path' in request.POST:
+                ckt_file = CircuitSchematics()
+                ckt_file.ckt_file_name = request.POST.getlist(u'ckt_file_path')[0]
+                if u'ckt_file_descrip' in request.POST:
+                    ckt_file.ckt_file_descrip = request.POST.getlist(u'ckt_file_descrip')[0]
+                ckt_file.ckt_sim_case = sim_para_model
+                ckt_file.save()
+    
+    
+            simulation_form = []
+            simulation_form.append(sim_para_model)
+            simulation_form.append([])
+            ckt_file_list = sim_para_model.circuitschematics_set.all()
+            ckt_schematic_form = []
+            for ckt_file_item in ckt_file_list:
+                ckt_schematic_form.append([ckt_file_item, CircuitSchematicsForm(instance=ckt_file_item)])
+            ckt_schematic_form.append([[], CircuitSchematicsForm()])
 
-                if u'ckt_file_path' in request.POST:
-                    ckt_file = CircuitSchematics()
-                    ckt_file.ckt_file_name = request.POST.getlist(u'ckt_file_path')[0]
-                    if u'ckt_file_descrip' in request.POST:
-                        ckt_file.ckt_file_descrip = request.POST.getlist(u'ckt_file_descrip')[0]
-                    ckt_file.ckt_sim_case = sim_para_model
-                    ckt_file.save()
-
-
-                simulation_form = []
-                simulation_form.append(SimulationCaseForm(instance=sim_para_model))
-                ckt_file_list = sim_para_model.circuitschematics_set.all()
-                ckt_schematic_form = []
-                for ckt_file_item in ckt_file_list:
-                    ckt_schematic_form.append([ckt_file_item, CircuitSchematicsForm(instance=ckt_file_item)])
+        elif "save_sim_param" in request.POST and request.POST["save_sim_param"]=="Save Simulation Parameters":
+            sim_id, simulation_form = extract_simulation_case(request)
+            ckt_schematic_form = []
+            if sim_id>0:
                 ckt_schematic_form.append([[], CircuitSchematicsForm()])
-
-        elif "submit" in request.POST and request.POST["submit"]=="Submit":
-            sim_para_received = SimulationCaseForm(request.POST)
-            if sim_para_received.is_valid():
-                sim_parameters = sim_para_received.cleaned_data
-                if "sim_id" in request.POST:
-                    sim_id = int(request.POST["sim_id"])
-                    if sim_id>0:
-                        sim_para_model = SimulationCase.objects.get(id=sim_id)
-                    else:
-                        sim_para_model = SimulationCase()
-                sim_para_model.sim_title = sim_parameters["sim_title"]
-                sim_para_model.sim_descrip = sim_parameters["sim_descrip"]
-                sim_para_model.sim_time_limit = sim_parameters["sim_time_limit"]
-                sim_para_model.sim_time_data = sim_parameters["sim_time_data"]
-                sim_para_model.sim_output_file = sim_parameters["sim_output_file"]
-                sim_para_model.sim_output_slice = sim_parameters["sim_output_slice"]
-                sim_para_model.sim_div_number = sim_parameters["sim_div_number"]
-                sim_para_model.sim_working_directory = sim_parameters["sim_working_directory"]
-                sim_para_model.save()
-                sim_id = sim_para_model.id
-
-                if u'ckt_file_path' in request.POST:
-                    ckt_file = CircuitSchematics()
-                    ckt_file.ckt_file_name = request.POST.getlist(u'ckt_file_path')[0]
-                    if u'ckt_file_descrip' in request.POST:
-                        ckt_file.ckt_file_descrip = request.POST.getlist(u'ckt_file_descrip')[0]
-                    ckt_file.ckt_sim_case = sim_para_model
-                    ckt_file.save()
-
-
-                simulation_form = []
-                simulation_form.append(SimulationCaseForm(instance=sim_para_model))
-                ckt_file_list = sim_para_model.circuitschematics_set.all()
-                ckt_schematic_form = []
-                for ckt_file_item in ckt_file_list:
-                    ckt_schematic_form.append([ckt_file_item, CircuitSchematicsForm(instance=ckt_file_item)])
-
-                if "\\" in sim_para_model.sim_working_directory:
-                    os_type = "win"
-                elif "/" in sim_para_model.sim_working_directory:
-                    os_type = "unix"
-
-                f = open(os.path.join(os.sep, \
-                                      sim_para_model.sim_working_directory, \
-                                      ckt_file_list[0].ckt_file_name), "r")
-                for line in f:
-                    print(line)
+        
+        elif "edit_sim_param" in request.POST and request.POST["edit_sim_param"]=="Edit Simulation Parameters":
+            if "sim_id" in request.POST:
+                sim_id = int(request.POST["sim_id"])
+                if sim_id>0:
+                    sim_para_model = SimulationCase.objects.get(id=sim_id)
+                else:
+                    sim_para_model = SimulationCase()
             
-            else:
-                sim_id = -1
-                simulation_form = []
-                simulation_form.append(SimulationCaseForm(request.POST))
-                ckt_schematic_form = []
-                ckt_schematic_form.append([[], CircuitSchematicsForm()])
-                
+            simulation_form = []
+            simulation_form.append([])
+            simulation_form.append(SimulationCaseForm())
+            ckt_schematic_form = []
 
         else:
             if "sim_id" in request.POST:
